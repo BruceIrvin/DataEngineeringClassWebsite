d817f6206c1b09eb1d1106bc21b957fbeb1e7edf
amans330 <amans330@gmail.com>
2021-01-29 21:58:56 -0800

    remove utf8

--- mysite/dataengineering/views.py
+++ mysite/dataengineering/views.py
@@ -8,7 +8,7 @@ def getBreadCrumbData(request):
     courseStartDate = datetime(2021, 1, 11)  # (yyyy-m-d) date on which first file is served
     today = datetime.today()
     if today < courseStartDate:
-        return HttpResponse('<h1>Course has not yet begun.</h1>'.encode("utf-8"))
+        return HttpResponse('<h1>Course has not yet begun.</h1>')
     file_number = (today - courseStartDate).days + 2
     filename = 'day%s' % file_number + '.json'
     file_path = os.path.join('dataengineering', 'static', 'dataengineering', filename)
@@ -17,7 +17,7 @@ def getBreadCrumbData(request):
             response = HttpResponse(fh.read())
             response['Content-Type'] = 'application/json'
             # https://code.djangoproject.com/ticket/31091 (for connection drops)
-            response['Connection'] = 'close'
+            # response['Connection'] = 'close'
             response['Content-Disposition'] = 'attachment; filename=%s'%(datetime.today().strftime('%Y-%m-%d'))+'.json'
             return response
     raise Http404
@@ -26,8 +26,8 @@ def getCadData(request):
     courseStartDate = datetime(2021, 1, 11)  # (yyyy-m-d) date on which first file is served
     today = datetime.today()
     if today < courseStartDate:
-        return HttpResponse('<h1>Course has not yet begun.</h1>'.encode("utf-8"))
-    file_number = (today - courseStartDate).days + 1
+        return HttpResponse('<h1>Course has not yet begun.</h1>')
+    file_number = (today - courseStartDate).days + 7
     filename = 'cad_table_day_%s' % file_number + '.html'
     file_path = os.path.join('dataengineering', 'templates', filename) # may be filepath?
     if os.path.exists(file_path):
